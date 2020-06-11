6aac5e33856ea72809ae41d08695d2b7f4786cde
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2020-06-11 17:29:00 +0500

    Update script.js

--- agcal/js/script.js
+++ agcal/js/script.js
@@ -23,7 +23,19 @@ function calculateAggregate() {
   var matricT = Number(document.getElementById('matric-marks-T').value);
   var hafiz = Boolean(document.getElementById('hafiz').checked);
 
+  //validation
+  valid = true;
+  if(entry < 0 || entry > 140 || interO < 0 || interT < 0 || matricO < 0
+  matricT < 0) {
+    valid = false;
+  }
+
   //now output result
-  document.getElementById('answer').innerHTML = formula(matricO,
+  if(valid === true) {
+    document.getElementById('answer').innerHTML = formula(matricO,
      matricT, interO, interT, entry, hafiz);
+  }
+  else {
+    document.getElementById('answer').innerHTML = "Invalid Input!";
+  }
 }
